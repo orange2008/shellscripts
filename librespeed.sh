6402bcaed790a02d10e1cdc2cdaec73b2741d630
orange2008 <40315138+orange2008@users.noreply.github.com>
2021-06-06 10:31:00 +0800

    Added LibreSpeed Docker

---
+++ librespeed.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "Installing Docker.."
+echo "- curl -fsSL https://get.docker.com | sudo bash"
+curl -fsSL https://get.docker.com | sudo bash
+echo "Pulling Image.."
+echo "- docker pull adolfintel/speedtest"
+docker pull adolfintel/speedtest
+echo "Running container.."
+echo "- docker run -d -p 8080:80 adolfintel/speedtest:latest"
+docker run -d -p 8080:80 adofintel/speedtest.latest
+echo "Done."
+echo "Visit http://[SERVER_IP]:8080 please."
