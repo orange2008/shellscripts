3dff7e7d6746b65ca0500f3ccfb924864718b29a
orange2008 <40315138+orange2008@users.noreply.github.com>
2021-06-06 10:37:18 +0800

    Fix .latest => :latest

--- librespeed.sh
+++ librespeed.sh
@@ -7,6 +7,6 @@ echo "- docker pull adolfintel/speedtest"
 docker pull adolfintel/speedtest
 echo "Running container.."
 echo "- docker run -d -p 8080:80 adolfintel/speedtest:latest"
-docker run -d -p 8080:80 adofintel/speedtest.latest
+docker run -d -p 8080:80 adofintel/speedtest:latest
 echo "Done."
 echo "Visit http://[SERVER_IP]:8080 please."
