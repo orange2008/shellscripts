4489ab4e3898ec2fd29225ba36a9482ce2792e88
orange2008 <40315138+orange2008@users.noreply.github.com>
2021-06-28 16:02:54 +0800

    Added H5AI

---
+++ h5ai.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo "You should install docker first."
+echo "Port:"
+read PORT
+echo "Folder:"
+read DIR
+echo "ALL GOOD!"
+docker run -d -p $PORT:80 -v $DIR:/h5ai --restart=always --name h5ai ilemonrain/h5ai:full
+echo "VISIT http://{SERVER_IP}:$PORT"
+echo "RECOMMEND TO INSTALL IPSEC VPN!"
+echo "Using 'curl https://shell.frank-ruan.com/ipsec.sh > ipsec.sh && sudo bash ipsec.sh"
