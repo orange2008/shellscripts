12d7f0b9fab4a63827b5fab8417dab13f3d1c6c3
orange2008 <40315138+orange2008@users.noreply.github.com>
2021-10-17 12:35:33 +0800

    changed libsdl

--- pygame.sh
+++ pygame.sh
@@ -4,6 +4,6 @@ sudo pip install pygame
 sudo pip3 install pygame
 sudo apt install python-pygame -y
 sudo apt install libsdl-image1.2-dev libsdl2-dev libsdl-ttf2.0-dev -y
-sudo apt install libsdl-mix1.2-dev libportmidi-dev libswscale-dev  libsmpeg-dev libavformat-dev libavcodec-dev python-numpy
+sudo apt install libsdl-mixer1.2-dev libportmidi-dev libswscale-dev  libsmpeg-dev libavformat-dev libavcodec-dev python-numpy -y
 
 
