1e5ffc24f1542f92350b15018f9bfe07cb61ae67
orange2008 <40315138+orange2008@users.noreply.github.com>
2021-10-17 12:31:23 +0800

    Added Pygame install script

---
+++ pygame.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sudo apt install python3-dev mercurial -y
+sudo pip install pygame
+sudo pip3 install pygame
+sudo apt install python-pygame -y
+sudo apt install libsdl-image1.2-dev libsdl2-dev libsdl-ttf2.0-dev -y
+sudo apt install libsdl-mix1.2-dev libportmidi-dev libswscale-dev  libsmpeg-dev libavformat-dev libavcodec-dev python-numpy
+
+
