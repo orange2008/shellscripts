06598ae4ffba940929ea366d24e36188af6ef04d
orange2008 <rsboss01@outlook.com>
2022-07-23 19:24:33 +0800

    fixed stupid erros

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -7,14 +7,14 @@ wget https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/nasm-2.15.05.tar.bz2
 tar xjvf nasm-2.15.05.tar.bz2
 cd nasm-2.15.05
 ./autogen.sh
-PATH=”$HOME/bin:$PATH” ./configure –prefix=”$HOME/ffmpeg_build” –bindir=”$HOME/bin”
-make -j($nproc)
+PATH=”$HOME/bin:$PATH” ./configure -prefix=”$HOME/ffmpeg_build” -bindir=”$HOME/bin”
+make -j${nproc}
 sudo make install
 
 cd ~/ffmpeg_sources
-git -C x264 pull 2> /dev/null || git clone –depth 1 https://code.videolan.org/videolan/x264.git
+git -C x264 pull 2> /dev/null || git clone -depth 1 https://code.videolan.org/videolan/x264.git
 cd x264
-PATH=”$HOME/bin:$PATH” PKG_CONFIG_PATH=”$HOME/ffmpeg_build/lib/pkgconfig” ./configure –prefix=”$HOME/ffmpeg_build” –bindir=”$HOME/bin” –enable-static –enable-pic
+PATH=”$HOME/bin:$PATH” PKG_CONFIG_PATH=”$HOME/ffmpeg_build/lib/pkgconfig” ./configure -prefix=”$HOME/ffmpeg_build” -bindir=”$HOME/bin” -enable-static -enable-pic
 PATH=”$HOME/bin:$PATH” make -j$(nproc)
 sudo make install
 
@@ -28,30 +28,30 @@ PATH=”$HOME/bin:$PATH” make -j$(nproc)
 sudo make install
 
 cd ~/ffmpeg_sources
-git -C libvpx pull 2> /dev/null || git clone –depth 1 https://chromium.googlesource.com/webm/libvpx.git
+git -C libvpx pull 2> /dev/null || git clone -depth 1 https://chromium.googlesource.com/webm/libvpx.git
 cd libvpx
-PATH=”$HOME/bin:$PATH” ./configure –prefix=”$HOME/ffmpeg_build” –disable-examples –disable-unit-tests –enable-vp9-highbitdepth –as=yasm
+PATH=”$HOME/bin:$PATH” ./configure -prefix=”$HOME/ffmpeg_build” -disable-examples -disable-unit-tests -enable-vp9-highbitdepth -as=yasm
 PATH=”$HOME/bin:$PATH” make -j$(nproc)
 sudo make install
 
 cd ~/ffmpeg_sources
-git -C fdk-aac pull 2> /dev/null || git clone –depth 1 https://github.com/mstorsjo/fdk-aac
+git -C fdk-aac pull 2> /dev/null || git clone -depth 1 https://github.com/mstorsjo/fdk-aac
 cd fdk-aac
 autoreconf -fiv
-./configure –prefix=”$HOME/ffmpeg_build” –disable-shared
+./configure -prefix=”$HOME/ffmpeg_build” -disable-shared
 make -j$(nproc)
 sudo make install
 
 cd ~/ffmpeg_sources
-git -C opus pull 2> /dev/null || git clone –depth 1 https://github.com/xiph/opus.git
+git -C opus pull 2> /dev/null || git clone -depth 1 https://github.com/xiph/opus.git
 cd opus
 ./autogen.sh
-./configure –prefix=”$HOME/ffmpeg_build” –disable-shared
+./configure -prefix=”$HOME/ffmpeg_build” -disable-shared
 make -j$(nproc)
 sudo make install
 
 cd ~/ffmpeg_sources
-git -C aom pull 2> /dev/null || git clone –depth 1 https://aomedia.googlesource.com/aom
+git -C aom pull 2> /dev/null || git clone -depth 1 https://aomedia.googlesource.com/aom
 mkdir -p aom_build
 cd aom_build
 PATH=”$HOME/bin:$PATH” cmake -G “Unix Makefiles” -DCMAKE_INSTALL_PREFIX=”$HOME/ffmpeg_build” -DENABLE_TESTS=OFF -DENABLE_NASM=on ../aom
@@ -59,13 +59,13 @@ PATH=”$HOME/bin:$PATH” make -j$(nproc)
 sudo make install
 
 sudo apt-get install python3-pip -y
-pip3 install –user meson
+pip3 install -user meson
 
 cd ~/ffmpeg_sources
-git -C dav1d pull 2> /dev/null || git clone –depth 1 https://code.videolan.org/videolan/dav1d.git
+git -C dav1d pull 2> /dev/null || git clone -depth 1 https://code.videolan.org/videolan/dav1d.git
 mkdir -p dav1d/build
 cd dav1d/build
-meson setup -Denable_tools=false -Denable_tests=false –default-library=static .. –prefix “$HOME/ffmpeg_build” –libdir=”$HOME/ffmpeg_build/lib”
+meson setup -Denable_tools=false -Denable_tests=false -default-library=static .. -prefix “$HOME/ffmpeg_build” -libdir=”$HOME/ffmpeg_build/lib”
 ninja
 sudo ninja install
 
@@ -74,7 +74,7 @@ wget https://github.com/Netflix/vmaf/archive/v2.1.1.tar.gz
 tar xvf v2.1.1.tar.gz
 mkdir -p vmaf-2.1.1/libvmaf/build\
 cd vmaf-2.1.1/libvmaf/build
-meson setup -Denable_tests=false -Denable_docs=false –buildtype=release –default-library=static .. –prefix “$HOME/ffmpeg_build” –bindir=”../../../../bin” –libdir=”$HOME/ffmpeg_build/lib”
+meson setup -Denable_tests=false -Denable_docs=false -buildtype=release -default-library=static .. -prefix “$HOME/ffmpeg_build” -bindir=”../../../../bin” -libdir=”$HOME/ffmpeg_build/lib”
 ninja
 sudo ninja install
 
@@ -82,7 +82,7 @@ cd ~/ffmpeg_sources
 wget -O ffmpeg-snapshot.tar.bz2 https://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
 tar xjvf ffmpeg-snapshot.tar.bz2
 cd ffmpeg
-PATH=”$HOME/bin:$PATH” PKG_CONFIG_PATH=”$HOME/ffmpeg_build/lib/pkgconfig” ./configure –prefix=”$HOME/ffmpeg_build” –pkg-config-flags=”–static” –extra-cflags=”-I$HOME/ffmpeg_build/include” –extra-ldflags=”-L$HOME/ffmpeg_build/lib” –extra-libs=”-lpthread -lm” –ld=”g++” –bindir=”$HOME/bin” –enable-gpl –enable-gnutls –enable-libaom –enable-libass –enable-libfdk-aac –enable-libfreetype –enable-libmp3lame –enable-libopus –enable-libsvtav1 –enable-libdav1d –enable-libvorbis –enable-libvpx –enable-libx264 –enable-libx265 –enable-nonfree
+PATH=”$HOME/bin:$PATH” PKG_CONFIG_PATH=”$HOME/ffmpeg_build/lib/pkgconfig” ./configure -prefix=”$HOME/ffmpeg_build” -pkg-config-flags=”-static” -extra-cflags=”-I$HOME/ffmpeg_build/include” -extra-ldflags=”-L$HOME/ffmpeg_build/lib” -extra-libs=”-lpthread -lm” -ld=”g++” -bindir=”$HOME/bin” -enable-gpl -enable-gnutls -enable-libaom -enable-libass -enable-libfdk-aac -enable-libfreetype -enable-libmp3lame -enable-libopus -enable-libsvtav1 -enable-libdav1d -enable-libvorbis -enable-libvpx -enable-libx264 -enable-libx265 -enable-nonfree
 PATH=”$HOME/bin:$PATH” make -j$(nproc)
 sudo make install
 hash -r
