e40b3c7b8749bef43be3fb6bb5cab1566cd0aa3c
orange2008 <rsboss01@outlook.com>
2022-07-23 19:26:07 +0800

    fuck all the backslashes

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -72,7 +72,7 @@ sudo ninja install
 cd ~/ffmpeg_sources
 wget https://github.com/Netflix/vmaf/archive/v2.1.1.tar.gz
 tar xvf v2.1.1.tar.gz
-mkdir -p vmaf-2.1.1/libvmaf/build\
+mkdir -p vmaf-2.1.1/libvmaf/build
 cd vmaf-2.1.1/libvmaf/build
 meson setup -Denable_tests=false -Denable_docs=false -buildtype=release -default-library=static .. -prefix “$HOME/ffmpeg_build” -bindir=”../../../../bin” -libdir=”$HOME/ffmpeg_build/lib”
 ninja
