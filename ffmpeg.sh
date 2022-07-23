dad46007525456df5c5ac910f5b8c32eb217a5f4
orange2008 <rsboss01@outlook.com>
2022-07-23 19:17:35 +0800

    bugs

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo apt-get update -qq sudo apt-get -y install autoconf automake build-essential cmake git-core libass-dev libfreetype6-dev libgnutls28-dev libmp3lame-dev libsdl2-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev meson ninja-build pkg-config texinfo wget yasm zlib1g-dev
-sudo apt install libunistring-dev libaom-dev
+sudo apt install libunistring-dev libaom-dev -y
 mkdir -p ~/ffmpeg_sources ~/bin
 cd ~/ffmpeg_sources
 wget https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/nasm-2.15.05.tar.bz2
