c3ecf9005c3f5380e349bb0c24f75031535376e3
Frank Ruan <40315138+orange2008@users.noreply.github.com>
2022-08-02 14:20:30 +0800

    added zstd compilation script.

---
+++ zstd.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "Compiling zstd..."
+sudo apt update
+sudo apt install -y gcc g++ build-essential cmake git curl wget
+git clone https://github.com/facebook/zstd.git --depth=1
+cd zstd
+make -j$(nproc)
+sudo make install
