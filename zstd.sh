866ef0c078e8c87b6b3ae7abdffbef18ff2bea22
Frank Ruan <40315138+orange2008@users.noreply.github.com>
2022-08-08 19:11:18 +0800

    added zstd

--- zstd.sh
+++ zstd.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "Compiling zstd..."
 sudo apt update
-sudo apt install -y gcc g++ build-essential cmake git curl wget
+sudo apt install -y gcc g++ build-essential cmake git curl wget zlibg1 zlibg1-dev liblz4-dev liblzma-dev
 git clone https://github.com/facebook/zstd.git --depth=1
 cd zstd
 make -j$(nproc)
