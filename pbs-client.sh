cf2d77487bb6ed49b3e145bb09482562ae7a66ae
Frank Ruan <admin@frank-ruan.com>
2023-03-11 23:12:39 +0800

    added Proxmox Backup Client

---
+++ pbs-client.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+mkdir /etc/apt/sources.list.d
+wget -O /etc/apt/sources.list.d/pbs-client.list https://shell.frank-ruan.com/apt_sources/pbs-client.list
+cd /tmp
+wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_amd64.deb -O libssl1.deb
+apt install -y ./libssl1.deb
+wget http://enterprise.proxmox.com/debian/proxmox-release-bullseye.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bullseye.gpg
+apt update
+apt install proxmox-backup-client
+echo "\nDone."
